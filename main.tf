114d394fe71369be0473351086e1aefc1938a3ba
deal-machine <95deal@gmail.com>
2024-03-14 20:04:17 -0300

    chore: refactor infra to be abstract

--- main.tf
+++ main.tf
@@ -1,24 +1,13 @@
-provider "google" {
-  credentials = file("postech-415418-1cc45ad2638e.json")
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-}
-
-terraform {
-  backend "gcs" {
-    # bucket = "popostech"
-    # prefix = "terraform/db"
-  }
-}
-
 resource "google_sql_database_instance" "instance" {
-  name             = "postgres"
-  region           = var.region
-  database_version = "POSTGRES_14"
-  settings {
-    tier = "db-f1-micro"
+  name    = var.instance_name
+  region  = var.region
+  project = var.project_id
 
+  database_version    = var.postgres_version
+  deletion_protection = false
+
+  settings {
+    tier = var.machine_type
     ip_configuration {
       ipv4_enabled = true
       authorized_networks {
@@ -27,17 +16,22 @@ resource "google_sql_database_instance" "instance" {
       }
     }
   }
+  
+  depends_on = [google_project_service.sqladmin, google_project_service.cloudresourcemanager]
+}
 
-  deletion_protection = "false"
+resource "google_sql_database" "database" {
+  project    = var.project_id
+  name       = var.db_name
+  charset    = "utf8"
+  instance   = google_sql_database_instance.instance.name
+  depends_on = [google_sql_database_instance.instance]
 }
 
 resource "google_sql_user" "user" {
-  instance = google_sql_database_instance.instance.name
-  name     = var.db_username
-  password = var.db_password
-}
-
-output "db_instance_ip" {
-  value       = google_sql_database_instance.instance.ip_address[0].ip_address
-  description = "The public IP address of the database instance."
+  project    = var.project_id
+  name       = var.db_username
+  password   = var.db_pass
+  instance   = google_sql_database_instance.instance.name
+  depends_on = [google_sql_database_instance.instance]
 }
