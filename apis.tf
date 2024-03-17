d67a110fb806ac87f73633aff1e09240ac16f49b
deal-machine <95deal@gmail.com>
2024-03-17 10:35:15 -0300

    fix: apis dependency

--- apis.tf
+++ apis.tf
@@ -8,8 +8,3 @@ resource "google_project_service" "cloudresourcemanager" {
   service                    = "cloudresourcemanager.googleapis.com"
   disable_dependent_services = true
 }
-resource "google_project_service" "serviceusage" {
-  project                    = var.project_id
-  service                    = "serviceusage.googleapis.com"
-  disable_dependent_services = true
-}
